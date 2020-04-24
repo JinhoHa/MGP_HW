7f8f04c57146c1b5ab798245e82cb2914c5b6e59
JinhoHa <39648237+JinhoHa@users.noreply.github.com>
2020-04-25 02:14:50 +0900

    Update better_locked_linked_list.h

--- MGP/MGP/better_locked_linked_list.h
+++ MGP/MGP/better_locked_linked_list.h
@@ -1,3 +1,4 @@
+/*2017147594, Jinho Ha*/
 #ifndef _BETTER_LOCKED_LINKED_LIST_H_
 #define _BETTER_LOCKED_LINKED_LIST_H_
 
@@ -8,28 +9,70 @@
 
 // basic node structure
 class better_locked_linked_list : public linked_list {
-  class node {
-    // TODO
-  };
-  node *head;
- public:
-  better_locked_linked_list() {
-    // TODO
-  }
+	class node {
+		// TODO
+	public:
+		node(int key) : key(key) {
+			next = nullptr;
+		}
+		int key;
+		node* next;
+		// every node has mutex, so that every node can be locked/unlocked respectively
+		std::mutex node_mutex;
+	};
 
-  bool contains(int key) {
-		return false;
-    // TODO
-  }
+	node* head;
 
-  bool insert(int key) {
-		return false;
-    // TODO
-  }
+public:
+	better_locked_linked_list() {
+		// TODO
+		this->head = new node(0);
+	}
 
-  bool remove(int key) {
+	bool contains(int key) {
+		// TODO
+		node* prev = this->head;
+		prev->node_mutex.lock();
+		node* curr = prev->next;
+		if (curr) { curr->node_mutex.lock(); }
+		else {
+			prev->node_mutex.unlock();
+			return false;
+		}
+
+		// proceed with hand-by-hand locking
+		while (curr->key < key) {
+			// unlock the prev node first, and then lock the next node
+			prev->node_mutex.unlock();
+			prev = curr;
+			curr = curr->next;
+			if (curr) { curr->node_mutex.lock(); }
+			else {
+				prev->node_mutex.unlock();
+				return false;
+			}
+		}
+		// unlock the nodes before return
+		if (curr != nullptr && curr->key == key) {
+			prev->node_mutex.unlock();
+			curr->node_mutex.unlock();
+			return true;
+		}
+		else {
+			prev->node_mutex.unlock();
+			curr->node_mutex.unlock();
+			return false;
+		}
+	}
+
+	bool insert(int key) {
+		// TODO
+
+	}
+
+	bool remove(int key) {
 		return false;
-    // TODO
-  }
+		// TODO
+	}
 };
 #endif
