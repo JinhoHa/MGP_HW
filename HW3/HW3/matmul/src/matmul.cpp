fa9947edf9565a9ee04ff101521901996175907b
JinhoHa <39648237+JinhoHa@users.noreply.github.com>
2020-05-21 00:47:53 +0900

    ikj
    matmul_optimal took 2.43483 sec

--- HW3/HW3/matmul/src/matmul.cpp
+++ HW3/HW3/matmul/src/matmul.cpp
@@ -12,4 +12,12 @@ void matmul_ref(const int* const matrixA, const int* const matrixB,
 void matmul_optimized(const int* const matrixA, const int* const matrixB,
                       int* const matrixC, const int n) {
   // TODO: Implement your code
+  for(int i=0; i<n; i++) {
+    for(int k=0; k<n; k++) {
+      int a_ik = matrixA[i*n + k];
+      for(int j=0; j<n; j++) {
+        matrixC[i*n + j] += a_ik * matrixB[k*n + j];
+      }
+    }
+  }
 }
